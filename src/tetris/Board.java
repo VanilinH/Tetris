d2261fb2e8dc7d4105dacdbefdb879b20e23de58
Tetris <vanilin1403@gmail.com>
2026-01-21 09:29:57 +0200

    Updated

--- src/tetris/Board.java
+++ src/tetris/Board.java
@@ -1,25 +1,25 @@
 package tetris;
 
 public class Board {
-    private final int wigth = 10;
+    private final int width = 10;
     private final int height = 20;
     private int[][] grid;
 
     public Board() {
-        grid = new int[height][wigth];
+        grid = new int[height][width];
     }
 
     public void render(Tetromino piece) {
 
-        for (int i = 0; i < height; i++) System.println();
+        for (int i = 0; i < height; i++) System.out.println();
 
         for (int y = 0; y < height; y++) {
-            System.print("|");
+            System.out.print("|");
             for (int x = 0; x < width; x++) {
                 if (isPieceAt(x, y, piece)) {
                     System.out.print("[]");
-                } else if (grind[y][x] != 0) {
-                    System.out.print("##")
+                } else if (grid[y][x] != 0) {
+                    System.out.print("##");
                 } else {
                     System.out.print(" .");
                 }
@@ -29,9 +29,73 @@ public class Board {
             System.out.println("----------");
             }
             private boolean isPieceAt(int gridX, int gridY, Tetromino p) {
-                
+                int[][] shape = p.getShape();
+                int pieceX = p.getX();
+                int pieceY = p.getY();
+
+                if (gridX >= pieceX && gridX < pieceX + shape[0].length &&
+                    gridY >= pieceY && gridY < pieceY + shape.length) {
+                    return shape[gridY - pieceY][gridX - pieceX] != 0;
+                }
+                return false;
+            }
+
+            public boolean canMove(Tetromino p, int nextX, int nextY) {
+        int[][] shape = p.getShape();
+        for (int row = 0; row < shape.length; row++) {
+            for (int col = 0; col < shape[row].length; col++) {
+                if (shape[row][col] != 0) {
+                    int targetX = nextX + col;
+                    int targetY = nextY + row;
+
+                    if (targetY < 0) continue;
+
+                    if (targetX < 0 || targetX >= width || targetY >= height) {
+                        return false;
+
+                    if (grid[targetY][targetX] != 0) {
+                        return false;
+                    }
             }
         }
-            
+        return true;
+    }
+
+    public void freezePiece(Tetromino p) {
+        int[][] shape = p.getShape();
+        int pieceX = p.getX();
+        int pieceY = p.getY();
+
+        for (int row = 0; row < shape.length; row++) {
+            for (int col = 0; col < shape[row].length; col++) {
+                if (shape[row][col] != 0) {
+                    int targetX = pieceX + col;
+                    int targetY = pieceY + row;
+                    if (targetY >= 0 && targetY < height && targetX >= 0 && targetX < width) {
+                        grid[targetY][targetX] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    public void clearLines() {
+        for (int y = 0; y < height; y++) {
+            boolean fullLine = true;
+            for (int x = 0; x < width; x++) {
+                if (grid[y][x] == 0) {
+                    fullLine = false;
+                    break;
+                }
+            }
+            if (fullLine) {
+                for (int row = y; row > 0; row--) {
+                    System.arraycopy(grid[row - 1], 0, grid[row], 0, width);
+                }
+                for (int x = 0; x < width; x++) {
+                    grid[0][x] = 0;
+                }
+            }
+        }
     }
 }
