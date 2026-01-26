3173da291a39c11ce9f2041ae7e033930ec69179
Ivan Khlopchik <vanilin1403@gmail.com>
2026-01-26 10:46:47 +0200

    Update Tetromino.java

--- src/tetris/Tetromino.java
+++ src/tetris/Tetromino.java
@@ -1,44 +1,51 @@
 package tetris;
-
+// Class representing a Tetromino piece
 public class Tetromino {
     private int[][] shape; // shape array
     private int x, y; // position on the board
-
+// Constructor to initialize Tetromino with a shape
     public Tetromino(int[][] shape) {
         this.shape = shape;
         this.x = 4;
         this.y = 0;
     }
-
+// Move the piece down by one unit
     public void moveDown() {
         y++;
     }
+// Move the piece left by one unit
     public void moveLeft() {
         x--;
     }
+// Move the piece right by one unit
     public void moveRight() {
         x++;
     }
-
-    public void rotate() {
-        int n = shape.length;
-        int[][] rotated = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                rotated[j][n - 1 - i] = shape[i][j];
+// Rotate the piece clockwise
+    public Tetromino rotate() {
+        int rows = shape.length;
+        int cols = shape[0].length;
+        int[][] rotatedShape = new int[cols][rows];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                rotatedShape[j][rows - 1 - i] = shape[i][j];
             }
         }
-        shape = rotated;
+        Tetromino rotated = new Tetromino(rotatedShape);
+        rotated.x = this.x;
+        rotated.y = this.y;
+        return rotated;
     }
 
     public int[][] getShape() {
         return shape;
     }
+
     public int getX() {
         return x;
     }
+
     public int getY() {
         return y;
     }
-
 }
